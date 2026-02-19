7e4972b9d5dacb0faca9a0ab75ce5b07dc44bf81
Luis Rivera <leliezerrivera@gmail.com>
2026-02-19 00:16:24 -0600

    Se corrigio un error en pantalla.html

--- backend/app/routes/tickets.py
+++ backend/app/routes/tickets.py
@@ -502,15 +502,12 @@ def get_tickets_publico():
                 t.ID_Turno AS id_turno,
                 t.ID_Ventanilla AS id_ventanilla,
                 v.Ventanilla AS ventanilla,
-                a.Matricula AS matricula,
-                CONCAT(a.nombre1, ' ', a.Apellido1) AS nombre_alumno,
                 s.Sector AS sector,
                 et.Nombre AS estado,
                 et.ID_Estado AS estado_id,
                 t.Fecha_Ticket AS fecha_ticket,
                 'normal' AS tipo
             FROM Turno t
-            JOIN Alumnos a ON t.ID_Alumno = a.ID_Alumno
             JOIN Sectores s ON t.ID_Sector = s.ID_Sector
             JOIN Estados_Turno et ON t.ID_Estados = et.ID_Estado
             LEFT JOIN Ventanillas v ON t.ID_Ventanilla = v.ID_Ventanilla
