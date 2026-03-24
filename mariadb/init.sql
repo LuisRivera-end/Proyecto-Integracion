dc766f42c0e246b45527ed97719c241589f5b364
Luis Rivera <leliezerrivera@gmail.com>
2026-03-24 16:35:31 -0600

    Sin usuarios

--- mariadb/init.sql
+++ mariadb/init.sql
@@ -192,12 +192,6 @@ INSERT INTO Ventanillas (Ventanilla, ID_Sector) VALUES
 INSERT INTO Empleado (ID_ROL, nombre1, nombre2, Apellido1, Apellido2, Usuario, Passwd, ID_Estado)
 VALUES 
 (1, 'Luis', '', 'Rivera', '', 'admin', SHA2('12345678', 256), 1),
-(2, 'Luis', '', 'Rivera', 'Gamez', 'Fur1us', SHA2('12345678', 256), 1),
-(2, 'Gabriel', '', 'Ortiz', '', 'itzumi', SHA2('12345678', 256), 1),
-(3, 'Salvador', '', 'Butanda', '', 'python', SHA2('12345678', 256), 1),
-(4, 'Cristian', '', 'Butanda', '', 'butanda', SHA2('12345678', 256), 1),
-(2, 'Jose', 'Antonio', 'Zenil', 'Ruiz', 'antoniozr', SHA2('12345678', 256), 1),
-(5, 'Jaime', '', 'Mendez', '', 'jaime', SHA2('12345678', 256), 1);
 
 /* ====== 8. Rol_Ventanilla ====== */
 INSERT INTO Rol_Ventanilla (ID_Rol, ID_Ventanilla) VALUES
